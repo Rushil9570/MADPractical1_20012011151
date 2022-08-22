12724c98b496c84e66bc5b773baa9fd5a45d6c5f
C2IN <rushilkakadiya115@gmail.com>
2022-08-22 21:31:11 +0530

    hh

---
+++ src/main/kotlin/practical7.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val number = 6
+    val factorial = fact(number)
+    println("Factorial of $number = $factorial")
+}
+
+tailrec fun fact(n: Int, temp: Int = 1): Int {
+    return if (n == 1){
+        temp
+    } else {
+        fact(n-1, temp*n)
+    }
+}
